9688d2bc20cb0548e90ff73a9a5a5958e5fc12c6
Jules Ibot <julzibot@MacBook-Air-de-Jules.local>
2023-11-17 18:32:26 +0100

    pull request for J's parsing

--- Jules/ConfigParse.cpp
+++ Jules/ConfigParse.cpp
@@ -1,26 +1,5 @@
 #include "ConfigParse.hpp"
 
-// ConfigParse parse_config_file(std::string path)
-// {
-//     std::ifstream conf_file(path);
-//     std::string line;
-//     std::string dir;
-//     size_t charpos;
-//     std::istringstream linestream("");
-//     ConfigParse config;
-
-//     while (std::getline(conf_file, line))
-//     {
-//         charpos = line.find('#');
-//         if (charpos != std::string::npos && line.find('"') > charpos)
-//         {
-//             line = line.substr(0, charpos);
-//         }
-//         linestream.str(line);
-//         linestream >> dir;
-//     }
-// }
-
 std::string parse_comments(std::string original_line)
 {
 	std::string line(original_line);
@@ -68,16 +47,80 @@ std::string parse_comments(std::string original_line)
 	return (line);
 }
 
+template <typename T>
+void	get_braces_content(std::string dir_key, T &stream, std::map<std::string, std::string> &directives)
+{
+	int open_braces = 1;
+	std::string line;
+
+	// std::cout << "BRACE" << std::endl;
+	while (std::getline(stream, line) && open_braces)
+	{
+		line = parse_comments(line);
+		if (line.find('{') != std::string::npos)
+			open_braces++;
+		else if (line.find('}') != std::string::npos)
+			open_braces--;
+		if (open_braces)
+			directives[dir_key] += line + "\n";
+	}
+	if (open_braces)
+		throw std::exception();
+}
+
+void parse_config_file(std::string path)
+{
+	bool dir = 0;
+	int	loop = 0;
+    std::ifstream conf_file(path);
+    std::string line;
+    std::string directive = "none";
+    std::istringstream ls;
+    size_t bracepos;
+	std::map<std::string, std::string> directives;
+	std::map<std::string, std::string>::iterator dir_it = directives.begin();
+    ConfigParse config;
+
+    while (std::getline(conf_file, line) || std::getline(ls, line) || (dir && dir_it != directives.end()))
+    {
+		loop++;
+		if (!dir && directives.begin() != directives.end())
+		{
+			dir_it = directives.begin();
+			dir = 1;
+		}
+		if (conf_file.eof() && ls.eof())
+		{
+			directive = dir_it->first;
+			ls.str(dir_it->second);
+			// std::cout << ls.str() << "loop: " << loop << std::endl;
+			std::getline(ls, line);
+			// std::cout << line << "loop: " << loop << std::endl;
+			dir_it++;
+		}
+		line = parse_comments(line);
+		//Milan's function
+		bracepos = line.find('{');
+		if (bracepos != std::string::npos && !conf_file.eof())
+			get_braces_content<std::ifstream>(line.substr(0, bracepos), conf_file, directives);
+		else if (bracepos != std::string::npos && conf_file.eof())
+			get_braces_content(line.substr(0, bracepos), ls, directives);
+		// parse_config_line(line, directive, config);
+    }
+	// std::cout << "end" << std::endl;
+	// for (dir_it = directives.begin(); dir_it != directives.end(); dir_it++)
+	// 	std::cout << "key: " << dir_it->first << "  value: " << dir_it->second << std::endl;
+}
+
 int main()
 {
-	std::string str =	"here \'\" is \"#stuff\' good #\"right ?\n"
-						"somestuff #\"yeah\"\n"
-						"plus#morestufff\n"
-						"##ok\n"
-						"it works";
+	std::string path = "./webserv.conf";
 	std::string line;
-	std::istringstream ss = std::istringstream(str);
-	while (std::getline(ss, line))
-		std::cout << "original line: " << line << std::endl << "parsed comments: " << parse_comments(line) << std::endl;
+	std::istringstream ls("Hello Milan\n"
+						"how are you ?");
+
+	parse_config_file(path);
+	// while (std::getline(ls, line))
+	// 	std::cout << line << std::endl;
 	return (0);
 }
--- Jules/ConfigParse.hpp
+++ Jules/ConfigParse.hpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <unordered_map>
+#include <map>
 #include <forward_list>
 
 class ConfigServer
